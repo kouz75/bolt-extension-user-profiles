14c37640b6574dab2879d3bb47e4ab8c9437bfdc
Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
2016-04-12 20:18:59 +0200

    implementing public profiles permissions

--- Extension.php
+++ Extension.php
@@ -209,8 +209,8 @@ class Extension extends BaseExtension
         }
 
         // handle weird weirdness with weird input fields like checkboxes
-        foreach($this->config['fields'] as $key => $field){
-            if($field['type'] === "checkbox"){
+        foreach ($this->config['fields'] as $key => $field) {
+            if ($field['type'] === "checkbox") {
                 $data[$key] = $data[$key] ? 1 : 0;
             }
         }
@@ -282,9 +282,52 @@ class Extension extends BaseExtension
         return '';
     }
 
+    /**
+     * Determine if the given user has a public profile
+     * Used as Twig function and for internal checks
+     * Decides based on the configured permissions in the extension config
+     *
+     * @param array $user
+     * @return bool
+     */
     public function hasProfile(array $user)
     {
-        return $this->config['profiles']['enabled'];
+        if (!$this->config['profiles']['enabled']) {
+            return false;
+        }
+
+        $permissionConfig = $this->config['profiles']['permissions'];
+
+        // because of BC
+        if ($permissionConfig) {
+            $hasProfile = false;
+
+            // check if user has at least one of the defined roles
+            foreach ($permissionConfig['roles'] as $role) {
+                if (in_array($role, array_values($user['roles']), true)) {
+                    $hasProfile = true;
+                }
+            }
+
+            // user has none of the required roles, so we don't have to check the other things.
+            if (!$hasProfile) {
+                return false;
+            }
+
+            // check if username is one of the defined excluded usernames.
+            // we return false because if user is on this list, he isn't allowed to have a profile at all.
+            if (in_array($user['username'], $permissionConfig['excluded_usernames'], true)) {
+                return false;
+            }
+
+            if ($permissionConfig['conditional_field']) {
+                $hasProfile = $user[$permissionConfig['conditional_field']] ? true : false;
+            }
+
+            return $hasProfile;
+        }
+
+        return true;
     }
 
     /**
